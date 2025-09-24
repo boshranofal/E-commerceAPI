0096f568365d0fca8714c68fe9f30b8380deadad
boshranofal <boshranofal66@gmail.com>
2025-09-24 12:13:55 +0300

    Add product review feature and link it with users

---
+++ E-commerceAPI.DAL/DTO/Response/ReviewResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerceAPI.DAL.DTO.Response
+{
+    public class ReviewResponse
+    {
+        public int Id { get; set; }
+        public string Rate { get; set; }
+        public string Comment { get; set; }
+        public string FullNmae { get; set; }
+    }
+}
